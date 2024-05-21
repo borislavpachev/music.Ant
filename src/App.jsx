5ce191904f2bf25ce1625f6b9aa7031cefb903d8
borislavpachev <borislav.pachev@gmail.com>
2024-05-21 20:17:37 +0300

    search and results on user search request added

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import Header from './components/Header/Header';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import 'bootstrap/dist/css/bootstrap.min.css';
 import Home from './views/Home/Home';
 import ErrorPage from './views/ErrorPage/ErrorPage';
 import { useContext, useEffect, useState } from 'react';
@@ -8,11 +7,15 @@ import { ThemeContext } from './contexts/theme';
 import About from './views/About/About';
 import hash from './hash';
 import { getUserData } from './services/auth.service';
+import { spotifyApi } from './services/spotify.service';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   const [{ theme }] = useContext(ThemeContext);
   const [token, setToken] = useState(null);
   const [user, setUser] = useState(null);
+  const [search, setSearch] = useState('');
+  const [searchResults, setSearchResults] = useState([]);
 
   useEffect(() => {
     let _token = hash.access_token;
@@ -35,6 +38,30 @@ function App() {
     }
   }, []);
 
+  useEffect(() => {
+    if (!token) return;
+    spotifyApi.setAccessToken(token);
+  }, [token]);
+
+  useEffect(() => {
+    if (!search) return setSearchResults([]);
+    if (!token) return;
+    spotifyApi.searchTracks(search).then((res) => {
+      setSearchResults(
+        res.body.tracks.items.map((track) => {
+          return {
+            artist: track.artists[0].name,
+            trackName: track.name,
+            uri: track.uri,
+            albumURL: track.album.images[0].url,
+          };
+        })
+      );
+    });
+  }, [search, token]);
+
+  console.log(searchResults);
+
   const handleLogout = () => {
     setToken(null);
     setUser(null);
@@ -45,7 +72,12 @@ function App() {
     <>
       <main className={`app bg-${theme.color} text-${theme.textColor}`}>
         <BrowserRouter>
-          <Header user={user} logout={handleLogout} />
+          <Header
+            user={user}
+            logout={handleLogout}
+            search={search}
+            setSearch={setSearch}
+          />
           <Routes>
             <Route index element={<Home />} />
             <Route path="/home" element={<Home />} />
