5c005dc8d234617b02b4268c9aac6459057c3524
borislavpachev <borislav.pachev@gmail.com>
2024-05-21 20:16:58 +0300

    header props added for search

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -3,33 +3,36 @@ import { ThemeContext } from '../../contexts/theme';
 import { FiSun } from 'react-icons/fi';
 import { FiMoon } from 'react-icons/fi';
 import { NavLink } from 'react-router-dom';
-import Login from '../../views/Login/Login';
+import Login from '../Login/Login';
 import { PropTypes } from 'prop-types';
 import './Header.css';
 
-export default function Header({ user, logout }) {
+export default function Header({ user, logout, search, setSearch }) {
   const [{ theme, isDark }, toggleTheme] = useContext(ThemeContext);
 
   return (
-    <header
-      className={`d-flex justify-content-between align-items-center
+    <nav
+      className={`navbar navbar-expand-lg d-flex justify-content-between align-items-center
     p-3 bg-${theme.color} text-${theme.textColor} ${
         isDark ? 'header-dark' : 'header-light'
       }`}
     >
-      <input
-        type="search"
-        className="form-control w-50"
-        placeholder="Artists, Songs..."
-      />
       <div
         className={`d-flex bg-${theme.color === 'dark' ? 'light' : 'light'} 
-      rounded align-items-center`}
+  rounded align-items-center`}
       >
         <NavLink to="/home">
           <img src="/ant_logo.png" alt="logo" />
         </NavLink>
       </div>
+      <input
+        type="search"
+        className="form-control mx-4 fs-5"
+        placeholder="Artists, Songs..."
+        value={search}
+        onChange={(e) => setSearch(e.target.value)}
+      />
+
       <div className="d-flex gap-2 align-items-center">
         {!user ? (
           <Login />
@@ -53,11 +56,13 @@ export default function Header({ user, logout }) {
           {isDark ? <FiSun /> : <FiMoon />}
         </button>
       </div>
-    </header>
+    </nav>
   );
 }
 
 Header.propTypes = {
   user: PropTypes.object,
   logout: PropTypes.func,
+  search: PropTypes.string,
+  setSearch: PropTypes.func,
 };
