217107002da7fe44b93ae36f8f8953f547eee995
borislavpachev <borislav.pachev@gmail.com>
2024-05-21 16:58:33 +0300

    Login component added

---
+++ src/views/Login/Login.jsx
@@ -0,0 +1,19 @@
+import { Link } from 'react-router-dom';
+import {
+  authEndpoint,
+  clientId,
+  redirectUri,
+  scopes,
+} from '../../spotify.config';
+
+export default function Login() {
+  return (
+    <Link
+      to={`${authEndpoint}?client_id=${clientId}&redirect_uri=${redirectUri}&scope=${scopes.join(
+        '%20'
+      )}&response_type=token&show_dialog=true`}
+    >
+      <button className="btn btn-success">Login to Spotify</button>
+    </Link>
+  );
+}
