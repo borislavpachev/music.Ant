72b35ee9cd26c7584e0037df4aff1cdef38c59af
borislavpachev <borislav.pachev@gmail.com>
2024-05-21 16:58:08 +0300

    config files added for communication with the api

---
+++ src/spotify.config.js
@@ -0,0 +1,10 @@
+export const authEndpoint = 'https://accounts.spotify.com/authorize';
+export const clientId = '064aedda09a948809602155be5425651';
+export const redirectUri = 'http://localhost:5173/';
+export const scopes = [
+    'user-read-currently-playing',
+    'user-read-playback-state',
+    'user-top-read',
+    'user-read-recently-played',
+    'user-library-read'
+  ];
