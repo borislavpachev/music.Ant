7501e58bfd026675175acb4dc2911201be565186
borislavpachev <borislav.pachev@gmail.com>
2024-05-22 16:29:57 +0300

    spotify web playback added

--- src/spotify.config.js
+++ src/spotify.config.js
@@ -3,9 +3,13 @@ export const clientId = '064aedda09a948809602155be5425651';
 export const clientSecret = 'a841fa0df7454b72b2f6a641a9c56e22';
 export const redirectUri = 'http://localhost:5173/';
 export const scopes = [
-    'user-read-currently-playing',
-    'user-read-playback-state',
-    'user-top-read',
-    'user-read-recently-played',
-    'user-library-read'
-  ];
+  'streaming',
+  'user-read-currently-playing',
+  'user-read-playback-state',
+  'user-top-read',
+  'user-library-read',
+  'user-library-modify',
+  'user-read-email',
+  'user-read-private',
+];
+
