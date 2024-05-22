078f5947f3bae2293f2a7133523391a946f13762
borislavpachev <borislav.pachev@gmail.com>
2024-05-22 16:31:00 +0300

    music player added

---
+++ src/components/MusicPlayer/MusicPlayer.jsx
@@ -0,0 +1,34 @@
+import { useEffect, useState } from 'react';
+import SpotifyPlayer from 'react-spotify-web-playback';
+import { PropTypes } from 'prop-types';
+import './MusicPlayer.css';
+
+export default function MusicPlayer({ uri, setTrack }) {
+  const [token, setToken] = useState(null);
+
+  useEffect(() => {
+    let authToken = localStorage.getItem('spotifyToken');
+    setToken(authToken);
+  }, []);
+
+  if (!token) return;
+  return (
+    <div className="music-player d-flex flex-column bg-success align-items-end">
+      <SpotifyPlayer
+        token={token}
+        play={true}
+        showSaveIcon={true}
+        uris={uri ? [uri] : []}
+        styles={{ sliderColor: 'green#00B500' }}
+      />
+      <button className="btn btn-danger m-2" onClick={() => setTrack(null)}>
+        Close
+      </button>
+    </div>
+  );
+}
+
+MusicPlayer.propTypes = {
+  uri: PropTypes.string,
+  setTrack: PropTypes.func,
+};
--- src/views/Home/Home.jsx
+++ src/views/Home/Home.jsx
@@ -1,18 +1,37 @@
 import { PropTypes } from 'prop-types';
-import TrackCard from '../../components/TrackCard/TrackCard';
+import TrackMusicCard from '../../components/TrackMusicCard/TrackMusicCard';
+import { useState } from 'react';
+import MusicPlayer from '../../components/MusicPlayer/MusicPlayer';
 
 export default function Home({ results }) {
+  const [currentlyPlayingTrack, setCurrentlyPlayingTrack] = useState(null);
+
+  const selectTrack = (track) => {
+    setCurrentlyPlayingTrack(track);
+  };
+
   return (
     <div
       className={`d-flex flex-column justify-content-center align-items-center
   `}
     >
-      <h1>Home</h1>
       <div className="row align-items-center justify-content-center">
         {results.map((result, index) => {
-          return <TrackCard result={result} key={index} />;
+          return (
+            <TrackMusicCard
+              result={result}
+              key={index}
+              selectTrack={selectTrack}
+            />
+          );
         })}
       </div>
+      {!currentlyPlayingTrack ? null : (
+        <MusicPlayer
+          uri={currentlyPlayingTrack}
+          setTrack={setCurrentlyPlayingTrack}
+        />
+      )}
     </div>
   );
 }
