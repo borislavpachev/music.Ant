738a5615ebedc08f4e77d68ca65078a6d56d8edb
borislavpachev <borislav.pachev@gmail.com>
2024-05-22 17:57:33 +0300

    dark mode css fix

--- src/components/MusicPlayer/MusicPlayer.jsx
+++ src/components/MusicPlayer/MusicPlayer.jsx
@@ -1,9 +1,11 @@
-import { useEffect, useState } from 'react';
+import { useContext, useEffect, useState } from 'react';
 import SpotifyPlayer from 'react-spotify-web-playback';
 import { PropTypes } from 'prop-types';
 import './MusicPlayer.css';
+import { ThemeContext } from '../../contexts/theme';
 
 export default function MusicPlayer({ uri, setTrack }) {
+  const [{ isDark }] = useContext(ThemeContext);
   const [token, setToken] = useState(null);
 
   useEffect(() => {
@@ -13,14 +15,20 @@ export default function MusicPlayer({ uri, setTrack }) {
 
   if (!token) return;
   return (
-    <div className="music-player d-flex flex-column bg-success 
-    align-items-end p-1">
+    <div
+      className="music-player d-flex flex-column bg-success 
+    align-items-end p-1"
+    >
       <SpotifyPlayer
         token={token}
         play={true}
         showSaveIcon={true}
         uris={uri ? [uri] : []}
-        styles={{ sliderColor: '#00B500' }}
+        styles={{
+          sliderColor: '#00B500',
+          bgColor: isDark ? '#343a40' : '#f8f9fa',
+          color: isDark ? '#f8f9fa' : '#343a40',
+        }}
       />
       <button className="btn btn-danger mt-1" onClick={() => setTrack(null)}>
         Close
