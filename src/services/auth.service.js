6fc3e9390c93605831cb70e97d0ed961d4205f7e
borislavpachev <borislav.pachev@gmail.com>
2024-05-22 10:34:33 +0300

    logout user if token expired

--- src/services/auth.service.js
+++ src/services/auth.service.js
@@ -1,8 +1,18 @@
+import toast from 'react-hot-toast';
+
 export const getUserData = async (token) => {
-  const response = await fetch('https://api.spotify.com/v1/me', {
-    headers: {
-      Authorization: `Bearer ${token}`,
-    },
-  });
-  return response.json();
+  try {
+    const response = await fetch('https://api.spotify.com/v1/me', {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    });
+    const result = await response.json();
+    if (!result.ok) {
+      localStorage.removeItem('spotifyToken');
+    }
+    return result;
+  } catch (error) {
+    toast.error(error.message);
+  }
 };
