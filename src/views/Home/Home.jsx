3b03689ef85481abe8d3878a9031b4b9503c0a80
borislavpachev <borislav.pachev@gmail.com>
2024-05-22 10:35:01 +0300

    home view loading tracks on search

--- src/views/Home/Home.jsx
+++ src/views/Home/Home.jsx
@@ -1,10 +1,22 @@
-export default function Home() {
+import { PropTypes } from 'prop-types';
+import TrackCard from '../../components/TrackCard/TrackCard';
+
+export default function Home({ results }) {
   return (
     <div
-      className={`d-flex justify-content-center align-items-center
+      className={`d-flex flex-column justify-content-center align-items-center
   `}
     >
       <h1>Home</h1>
+      <div className="row align-items-center justify-content-center">
+        {results.map((result, index) => {
+          return <TrackCard result={result} key={index} />;
+        })}
+      </div>
     </div>
   );
 }
+
+Home.propTypes = {
+  results: PropTypes.array,
+};
