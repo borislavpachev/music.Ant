d48681764abff8de20bfaa9021172c946af6027c
borislavpachev <borislav.pachev@gmail.com>
2024-05-23 18:01:17 +0300

    if no search results, return information text

--- src/views/Home/Home.jsx
+++ src/views/Home/Home.jsx
@@ -5,27 +5,36 @@ import MusicPlayer from '../../components/MusicPlayer/MusicPlayer';
 
 export default function Home({ results }) {
   const [currentlyPlayingTrack, setCurrentlyPlayingTrack] = useState(null);
-
+  
   const selectTrack = (track) => {
     setCurrentlyPlayingTrack(track);
   };
+
 
   return (
     <div
       className={`d-flex flex-column justify-content-center align-items-center
   `}
     >
-      <div className="row align-items-center justify-content-center">
-        {results.map((result, index) => {
-          return (
-            <TrackMusicCard
-              result={result}
-              key={index}
-              selectTrack={selectTrack}
-            />
-          );
-        })}
-      </div>
+      {results.length === 0 ? (
+        <div className="row align-items-center justify-content-center">
+          <div className="mt-5">
+            <h3>No search results</h3>
+          </div>
+        </div>
+      ) : (
+        <div className="row align-items-center justify-content-center">
+          {results.map((result, index) => {
+            return (
+              <TrackMusicCard
+                result={result}
+                key={index}
+                selectTrack={selectTrack}
+              />
+            );
+          })}
+        </div>
+      )}
       {!currentlyPlayingTrack ? null : (
         <MusicPlayer
           uri={currentlyPlayingTrack}
