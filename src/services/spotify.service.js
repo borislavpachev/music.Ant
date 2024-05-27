92b40ad299f1a682ca9f8ed491e7a311a4d7538a
borislavpachev <borislav.pachev@gmail.com>
2024-05-27 16:54:50 +0300

    redirect uri dinamically added

--- src/services/spotify.service.js
+++ src/services/spotify.service.js
@@ -1,7 +1,8 @@
 import SpotifyWebApi from 'spotify-web-api-node';
-import { clientId, clientSecret } from '../spotify.config';
+import { clientId, clientSecret, redirectUri } from '../spotify.config';
 
 export const spotifyApi = new SpotifyWebApi({
   clientId: clientId,
   clientSecret: clientSecret,
+  redirectUri: redirectUri,
 });
