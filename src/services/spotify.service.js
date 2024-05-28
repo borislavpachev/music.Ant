3483563ccd365b4fb1db16d7c9a687d7c734bd0f
borislavpachev <borislav.pachev@gmail.com>
2024-05-28 11:19:12 +0300

    refresh access token

--- src/services/spotify.service.js
+++ src/services/spotify.service.js
@@ -6,3 +6,12 @@ export const spotifyApi = new SpotifyWebApi({
   clientSecret: clientSecret,
   redirectUri: redirectUri,
 });
+
+export const refreshAccessToken = async (refreshToken) => {
+  try {
+    const data = spotifyApi.refreshAccessToken(refreshToken);
+    return data.body;
+  } catch (error) {
+    throw new Error('Could not refresh access token');
+  }
+};
