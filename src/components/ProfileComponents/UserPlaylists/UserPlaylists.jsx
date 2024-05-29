d9347aa47a1969bee04bd8ce97d42adc8786446b
borislavpachev <borislav.pachev@gmail.com>
2024-05-29 19:09:43 +0300

    css fixes

--- src/components/ProfileComponents/UserPlaylists/UserPlaylists.jsx
+++ src/components/ProfileComponents/UserPlaylists/UserPlaylists.jsx
@@ -1,10 +1,10 @@
-import { useContext, useEffect, useState } from 'react';
-import { ThemeContext } from '../../../contexts/theme';
+import { useEffect, useState } from 'react';
 import { PropTypes } from 'prop-types';
 import { getMyPlaylists } from '../../../services/music.service';
+import HorizontalScroll from '../../../hoc/HorizontalScroll';
+import PlaylistCard from '../../PlaylistCard/PlaylistCard';
 
 export default function UserPlaylists({ token }) {
-  const [{ theme, isDark }] = useContext(ThemeContext);
   const [playlists, setPlaylists] = useState(null);
 
   useEffect(() => {
@@ -24,45 +24,23 @@ export default function UserPlaylists({ token }) {
   }, [token]);
 
   return (
-    <div
-      className={`w-75 d-flex align-items-center rounded
-  ${
-    isDark
-      ? `profile-dark bg-${theme.color} text-${theme.textColor} border border-white`
-      : `profile-light bg-${theme.color} text-${theme.textColor} border border-dark`
-  }
-  `}
-    >
+    <>
       {!playlists ? (
-        <div className="container text-center p-5 my-2 fs-2">
+        <div className="w-100 p-5 my-2 mx-4 fs-2 border rounded">
           User has no playlists
         </div>
       ) : (
-        playlists.map((playlist, index) => {
-          return (
-            <div
-              key={index}
-              className="container d-flex flex-column text-center
-            align-items-center justify-content-start"
-              style={{
-                height: '165px',
-                width: '165px',
-              }}
-            >
-              <img
-                alt="playlist"
-                src={`${playlist.image}`}
-                style={{ width: '110px', height: '110px' }}
-              />
-              <div>
-                <div className="fs-5">{playlist.name}</div>
-                <div>Tracks: {playlist.tracksCount}</div>
+        <HorizontalScroll styleClasses={'d-flex'}>
+          {playlists.map((playlist, index) => {
+            return (
+              <div key={index} style={{ display: 'inline-block' }}>
+                <PlaylistCard playlist={playlist} />
               </div>
-            </div>
-          );
-        })
+            );
+          })}
+        </HorizontalScroll>
       )}
-    </div>
+    </>
   );
 }
 
