3a694367469feb604b843f3823be69a093e7e14b
borislavpachev <borislav.pachev@gmail.com>
2024-05-29 14:42:06 +0300

    User profile component added

---
+++ src/views/Profile/Profile.jsx
@@ -0,0 +1,58 @@
+import { useContext, useEffect, useState } from 'react';
+import { getUserData } from '../../services/auth.service';
+import { PropTypes } from 'prop-types';
+import toast from 'react-hot-toast';
+import { ThemeContext } from '../../contexts/theme';
+import ProfileCard from '../../components/ProfileComponents/ProfileCard/ProfileCard';
+import UserTopTracks from '../../components/ProfileComponents/UserTopTracks/UserTopTracks';
+import UserPlaylists from '../../components/ProfileComponents/UserPlaylists/UserPlaylists';
+import './Profile.css';
+
+export default function Profile({ token, logout }) {
+  const [{ theme, isDark }] = useContext(ThemeContext);
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    if (!token) return;
+
+    getUserData(token, logout)
+      .then((data) => setUser(data))
+      .catch((error) => {
+        toast.error(error.message);
+      });
+  }, [token]);
+
+  return (
+    <>
+      {user ? (
+        <div className="w-100 h-100 align-items-center">
+          <div
+            className="bg-warning d-flex align-items-center
+          justify-content-center gap-4 p-2 m-1"
+          >
+            <ProfileCard user={user} />
+            <UserPlaylists token={token} />
+          </div>
+          <UserTopTracks token={token} />
+        </div>
+      ) : (
+        <div
+          className={`fs-2 m-5 p-5 border text-center rounded
+        ${
+          isDark
+            ? `bg-${theme.color} text-${theme.textColor} border border-white`
+            : `bg-${theme.color} text-${theme.textColor} border border-dark`
+        }
+        `}
+        >
+          No user
+        </div>
+      )}
+    </>
+  );
+}
+
+Profile.propTypes = {
+  token: PropTypes.string,
+  logout: PropTypes.func,
+};
