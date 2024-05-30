8261d5de4304738ad48289a6be273b08a1039fe2
borislavpachev <borislav.pachev@gmail.com>
2024-05-30 15:12:09 +0300

    bootstrap js added for header to be able to collapse

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import './index.css';
 import { ThemeProvider } from './contexts/theme.jsx';
 
