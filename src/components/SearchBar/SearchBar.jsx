e1c1c5eb996a274500b7c8ba865018de6695661c
borislavpachev <borislav.pachev@gmail.com>
2024-05-31 15:54:24 +0300

    AppContext for  search and added. Using URL query params in case of search applied

--- src/components/SearchBar/SearchBar.jsx
+++ src/components/SearchBar/SearchBar.jsx
@@ -1,24 +1,50 @@
 import PropTypes from 'prop-types';
+import { useContext, useEffect } from 'react';
+import { AppContext } from '../../contexts/AppContext';
+import { useNavigate } from 'react-router-dom';
 
-export default function SearchBar({ search, setSearch, user }) {
-  return (
+export default function SearchBar({ user, query, setQuery }) {
+  const [{ setSearch }] = useContext(AppContext);
+    const navigate = useNavigate();
+
+  useEffect(() => {
+    setSearch(query);
+    if (!query) return;
+    if (query) {
+      navigate(`/search?query=${encodeURIComponent(query)}`);
+    }
+  }, [query, setSearch, navigate]);
+
+  const handleQuery = (e) => {
+    setQuery(e.target.value);
+  };
+
+  const handleClear = () => {
+    setQuery('');
+    setSearch('');
+    navigate('/search'); 
+  };
+
+    return (
     <div className="w-100">
       <form role="search">
         <input
           type="search"
           className="form-control mx-2 my-2 fs-5"
           placeholder="Artists, Songs..."
-          value={search}
-          onChange={(e) => setSearch(e.target.value)}
+          value={query}
+          onChange={handleQuery}
+          onReset={handleClear}
           aria-label="Search"
           disabled={!user ? true : false}
         />
       </form>
     </div>
   );
+
 }
 SearchBar.propTypes = {
-  search: PropTypes.string,
-  setSearch: PropTypes.func,
   user: PropTypes.object,
+  query:PropTypes.string,
+  setQuery: PropTypes.func,
 };
