4280b77e4649660b77f983bc49ec9d06bb2f021f
borislavpachev <borislav.pachev@gmail.com>
2024-05-31 15:47:35 +0300

    AppProvider added for context which brings state for search, searchResults and current track

--- src/main.jsx
+++ src/main.jsx
@@ -4,11 +4,14 @@ import App from './App.jsx';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import './index.css';
 import { ThemeProvider } from './contexts/theme.jsx';
+import AppProvider from './contexts/AppContext.jsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <ThemeProvider>
-      <App />
-    </ThemeProvider>
+    <AppProvider>
+      <ThemeProvider>
+        <App />
+      </ThemeProvider>
+    </AppProvider>
   </React.StrictMode>
 );
