5dc43c7afa7f47826d6fea0dd27eaa0aa589f08f
borislavpachev <borislav.pachev@gmail.com>
2024-05-31 15:54:56 +0300

    AppContext for  search and searchResults added. Using URL query params in case of search applied

---
+++ src/views/Search/Search.jsx
@@ -0,0 +1,92 @@
+import { PropTypes } from 'prop-types';
+import TrackMusicCard from '../../components/TrackMusicCard/TrackMusicCard';
+import { useContext, useEffect } from 'react';
+import { getSearchResults } from '../../services/music.service';
+import { AppContext } from '../../contexts/AppContext';
+import toast from 'react-hot-toast';
+
+export default function Search({ accessToken, logout }) {
+  const [
+    {
+      search,
+      setSearch,
+      searchResults,
+      setSearchResults,
+      setCurrentlyPlayingTrack,
+    },
+  ] = useContext(AppContext);
+
+  useEffect(() => {
+    const urlParams = new URLSearchParams(window.location.search);
+    const queryParams = urlParams.get('query');
+    if (queryParams) {
+      setSearch(queryParams);
+    }
+  }, []);
+
+  useEffect(() => {
+    if (!search) {
+      setSearchResults([]);
+      return;
+    }
+
+    getSearchResults(accessToken, search)
+      .then((results) => {
+        setSearchResults(
+          results.map((track) => {
+            return {
+              artist: track.artists[0].name,
+              trackName: track.name,
+              uri: track.uri,
+              albumCover: track.album.images[0].url,
+            };
+          })
+        );
+      })
+      .catch((error) => {
+        if (error.message.includes('expired')) {
+          logout();
+        }
+        toast.error(error.message);
+      });
+  }, [search, accessToken]);
+
+  const selectTrack = (track) => {
+    setCurrentlyPlayingTrack(track);
+  };
+
+  return (
+    <div
+      className={`d-flex flex-column justify-content-center align-items-center
+  `}
+    >
+      {searchResults.length === 0 ? (
+        <div className="w-100 align-items-center justify-content-center">
+          <div className="text-center rounded m-3 p-5 border">
+            <h3>No search results</h3>
+          </div>
+        </div>
+      ) : (
+        <div className="row align-items-center justify-content-center">
+          {searchResults.map((result, index) => {
+            return (
+              <TrackMusicCard
+                track={result}
+                key={index}
+                selectTrack={selectTrack}
+              />
+            );
+          })}
+        </div>
+      )}
+    </div>
+  );
+}
+
+Search.propTypes = {
+  accessToken: PropTypes.string,
+  setAccessToken: PropTypes.func,
+  setRefreshToken: PropTypes.func,
+  setExpiresIn: PropTypes.func,
+  logout: PropTypes.func,
+};
