2c1fefce80c070053dd5038c98b425a1e9b871d3
borislavpachev <borislav.pachev@gmail.com>
2024-06-02 10:43:17 +0300

    Tooltip position - top-start of the song title.

--- src/components/CustomTooltip/CustomTooltip.jsx
+++ src/components/CustomTooltip/CustomTooltip.jsx
@@ -4,14 +4,22 @@ import PropTypes from 'prop-types';
 export default function CustomTooltip({ text, tooltipText, font }) {
   return (
     <OverlayTrigger
-      placement="top"
+      placement="top-start"
       overlay={
         <Tooltip id={`tooltip-title`} style={{ position: 'fixed' }}>
           {tooltipText}
         </Tooltip>
       }
     >
-      <span style={{ cursor: 'pointer', fontSize: `${font}`, whiteSpace: 'nowrap'}}>{text}</span>
+      <span
+        style={{
+          cursor: 'pointer',
+          fontSize: `${font}`,
+          whiteSpace: 'nowrap',
+        }}
+      >
+        {text}
+      </span>
     </OverlayTrigger>
   );
 }
