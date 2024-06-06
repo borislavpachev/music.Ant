eb00485ba1bfb1d9260e9b7fab5562922991a603
borislavpachev <borislav.pachev@gmail.com>
2024-06-06 13:59:12 +0300

    responsiveness added to profile card component

--- src/components/ProfileComponents/ProfileCard/ProfileCard.jsx
+++ src/components/ProfileComponents/ProfileCard/ProfileCard.jsx
@@ -1,23 +1,23 @@
 import { PropTypes } from 'prop-types';
-import './ProfileCard.css';
 
 export default function ProfileCard({ user }) {
   return (
-    <div
-      className={`custom-profile-card d-flex align-items-center p-2 rounded bg-success`}
-    >
-      <img
-        src={`${user?.images[1].url}`}
-        style={{
-          height: '150px',
-          width: '150px',
-          borderRadius: '50%',
-        }}
-      />
-      <div className="d-flex flex-column align-items-center mx-3 text-white">
-        <h3>{user.display_name}</h3>
-        <span style={{ whiteSpace: 'nowrap' }}>Country: {user.country}</span>
-        <span>Followers: {user.followers.total}</span>
+    <div className='card bg-success' style={{ width: '400px' }}>
+      <div className="card-body d-flex align-items-center">
+        <img
+          src={`${user?.images[1].url}`}
+          style={{
+            borderRadius: '50%',
+            maxWidth: '50%',
+            height: 'auto',
+          }}
+          className="img-fluid"
+        />
+        <div className="d-flex flex-column align-items-center mx-3 text-white">
+          <p className="fs-4">{user.display_name}</p>
+          <span style={{ whiteSpace: 'nowrap' }}>Country: {user.country}</span>
+          <span>Followers: {user.followers.total}</span>
+        </div>
       </div>
     </div>
   );
