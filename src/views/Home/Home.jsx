7107577bf388ac5204eb814e05ab820b317a3547
borislavpachev <borislav.pachev@gmail.com>
2024-06-06 14:36:37 +0300

    responsiveness added

--- src/views/Home/Home.jsx
+++ src/views/Home/Home.jsx
@@ -1,12 +1,12 @@
 import { PropTypes } from 'prop-types';
 import { useContext, useEffect, useState } from 'react';
 import { getNewReleases } from '../../services/music.service';
-import { useNavigate } from 'react-router-dom';
-import { redirectUri } from '../../spotify.config';
 import TrackMusicCard from '../../components/TrackMusicCard/TrackMusicCard';
 import HorizontalScroll from '../../hoc/HorizontalScroll';
-import toast from 'react-hot-toast';
 import { AppContext } from '../../contexts/AppContext';
+import { useNavigate } from 'react-router-dom';
+import { redirectUri } from '../../spotify.config';
+import toast from 'react-hot-toast';
 
 export default function Home({
   accessToken,
@@ -21,6 +21,7 @@ export default function Home({
   useEffect(() => {
     const urlParams = new URLSearchParams(window.location.search);
     const code = urlParams.get('code');
+
     if (code) {
       getToken(code);
       navigate('/');
@@ -74,7 +75,7 @@ export default function Home({
 
         setAccessToken(data.access_token);
         setRefreshToken(data.refresh_token);
-        setExpiresIn(data.expires_in);
+        setExpiresIn(61);
       } else {
         console.error('Error fetching token:', data);
       }
@@ -89,81 +90,157 @@ export default function Home({
   };
 
   return (
-    <div
-      className={`custom-scroll row justify-content-center
-       align-items-center fs-5 py-5 
+    <div className="container">
+      <div
+        className={`custom-scroll row justify-content-center
+       align-items-center fs-5 py-5
     `}
-    >
-      <div className="w-100 align-items-center justify-content-center">
-        <HorizontalScroll styleClasses={'h-100 d-flex'}>
-          {!newReleases ? (
-            <div className="bg-white rounded p-3 mx-3 w-100 text-center">
-              <img
-                src="/ant_logo1.png"
-                alt="logo"
-                style={{ height: '70px', width: '70px' }}
-              />
-            </div>
-          ) : (
-            <>
-              <div
-                style={{ zIndex: 10001, left: 0 }}
-                className="position-sticky d-flex flex-column align-items-center
-                justify-content-center mt-2 p-3 bg-success me-3"
-              >
-                <h4 className="text-center text-white"> New Releases</h4>
+      >
+        <div className="w-100 align-items-center justify-content-center">
+          <HorizontalScroll styleClasses={'h-100 d-flex'}>
+            {!newReleases ? (
+              <div className="bg-white rounded p-3 mx-3 w-100 text-center">
+                <img
+                  src="/ant_logo1.png"
+                  alt="logo"
+                  style={{ height: '70px', width: '70px' }}
+                />
               </div>
-              {newReleases.map((track, index) => {
-                return (
-                  <div style={{ display: 'inline-block' }} key={index}>
-                    <TrackMusicCard
-                      track={track}
-                      key={index}
-                      selectTrack={selectTrack}
-                    />
-                  </div>
-                );
-              })}
-            </>
-          )}
-        </HorizontalScroll>
-      </div>
+            ) : (
+              <>
+                <div
+                  style={{ zIndex: 10001, left: 0 }}
+                  className="position-sticky d-flex flex-column align-items-center
+                justify-content-center mt-2 p-3 bg-success me-3"
+                >
+                  <h4 className="text-center text-white"> New Releases</h4>
+                </div>
+                {newReleases.map((track, index) => {
+                  return (
+                    <div style={{ display: 'inline-block' }} key={index}>
+                      <TrackMusicCard
+                        track={track}
+                        key={index}
+                        selectTrack={selectTrack}
+                      />
+                    </div>
+                  );
+                })}
+              </>
+            )}
+          </HorizontalScroll>
+        </div>
 
-      <div className="px-5 my-4">
-        Welcome to <strong>Music.ant</strong>, your ultimate destination for
-        searching and playing your favorite tracks seamlessly. Built with the
-        latest web technologies, Music.ant leverages the power of the Spotify
-        API and modern React frameworks to deliver a smooth and engaging music
-        experience.
-        <br />
-        <br />
-        <h5> Key Features:</h5>
-        <ul>
-          <li>
-            <strong>Track Search:</strong> Effortlessly search for songs,
-            artists, and albums using the extensive Spotify music library.
-          </li>
-          <li>
-            <strong>Instant Playback:</strong> Enjoy instant playback of tracks
-            directly within the app.
-          </li>
-          <li>
-            <strong>User-Friendly Interface:</strong> Navigate through a clean
-            and intuitive interface designed for an enjoyable user experience.
-          </li>
-        </ul>
-        <br />
-        <h5> How It Works:</h5>
-        <strong>You must have Spotify Premium Account. </strong>
-        <br />
-        Music.ant connects to the Spotify API display a wide range of tracks
-        based on your search queries. Simply login to your Spotify account and
-        type in the name of the song or artist you`re looking for, and Music.ant
-        will do the rest. Click on any track to start playing it instantly using
-        Spotify web player.
+        <div className="px-5 my-4">
+          Welcome to <strong>Music.ant</strong>, your ultimate destination for
+          searching and playing your favorite tracks seamlessly. Built with the
+          latest web technologies, Music.ant leverages the power of the Spotify
+          API and modern React frameworks to deliver a smooth and engaging music
+          experience.
+          <br />
+          <br />
+          <h5> Key Features:</h5>
+          <ul>
+            <li>
+              <strong>Track Search:</strong> Effortlessly search for songs,
+              artists, and albums using the extensive Spotify music library.
+            </li>
+            <li>
+              <strong>Instant Playback:</strong> Enjoy instant playback of
+              tracks directly within the app.
+            </li>
+            <li>
+              <strong>User-Friendly Interface:</strong> Navigate through a clean
+              and intuitive interface designed for an enjoyable user experience.
+            </li>
+          </ul>
+          <br />
+          <h5> How It Works:</h5>
+          <strong>You must have Spotify Premium Account. </strong>
+          <br />
+          Music.ant connects to the Spotify API display a wide range of tracks
+          based on your search queries. Simply login to your Spotify account and
+          type in the name of the song or artist you`re looking for, and
+          Music.ant will do the rest. Click on any track to start playing it
+          instantly using Spotify web player.
+        </div>
       </div>
     </div>
   );
+
+  // supposed to be responsive
+  // return (
+  //   <div className="container">
+  //     <div className="row justify-content-center align-items-center fs-5 py-2">
+  //       <div className="col-12 d-flex justify-content-center">
+  //         <HorizontalScroll styleClasses="d-flex">
+  //           {!newReleases ? (
+  //             <div className="bg-white rounded p-3 mx-3 w-100 text-center">
+  //               <img
+  //                 src="/ant_logo1.png"
+  //                 alt="logo"
+  //                 className="img-fluid"
+  //                 style={{ height: '70px', width: '70px' }}
+  //               />
+  //             </div>
+  //           ) : (
+  //             <>
+  //               <div
+  //                 style={{ zIndex: 10001, left: 0 }}
+  //                 className="position-sticky d-flex flex-column align-items-center justify-content-center mt-2 p-3 bg-success me-3"
+  //               >
+  //                 <h4 className="text-center text-white">New Releases</h4>
+  //               </div>
+  //               {newReleases.map((track, index) => (
+  //                 <div className="d-inline-block" key={index}>
+  //                   <TrackMusicCard
+  //                     track={track}
+  //                     key={index}
+  //                     selectTrack={selectTrack}
+  //                   />
+  //                 </div>
+  //               ))}
+  //             </>
+  //           )}
+  //         </HorizontalScroll>
+  //       </div>
+  //     </div>
+
+  //     <div className="px-5 my-4">
+  //       Welcome to <strong>Music.ant</strong>, your ultimate destination for
+  //       searching and playing your favorite tracks seamlessly. Built with the
+  //       latest web technologies, Music.ant leverages the power of the Spotify
+  //       API and modern React frameworks to deliver a smooth and engaging music
+  //       experience.
+  //       <br />
+  //       <br />
+  //       <h5>Key Features:</h5>
+  //       <ul>
+  //         <li>
+  //           <strong>Track Search:</strong> Effortlessly search for songs,
+  //           artists, and albums using the extensive Spotify music library.
+  //         </li>
+  //         <li>
+  //           <strong>Instant Playback:</strong> Enjoy instant playback of tracks
+  //           directly within the app.
+  //         </li>
+  //         <li>
+  //           <strong>User-Friendly Interface:</strong> Navigate through a clean
+  //           and intuitive interface designed for an enjoyable user experience.
+  //         </li>
+  //       </ul>
+  //       <br />
+  //       <h5>How It Works:</h5>
+  //       <strong>You must have a Spotify Premium Account.</strong>
+  //       <br />
+  //       Music.ant connects to the Spotify API to display a wide range of tracks
+  //       based on your search queries. Simply log in to your Spotify account and
+  //       type in the name of the song or artist you’re looking for, and Music.ant
+  //       will do the rest. Click on any track to start playing it instantly using
+  //       the Spotify web player.
+  //     </div>
+  //   </div>
+  // );
 }
 
 Home.propTypes = {
