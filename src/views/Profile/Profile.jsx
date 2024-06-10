87c27b4a04d7621a446ff297771d1fd366f33ac9
borislavpachev <borislav.pachev@gmail.com>
2024-06-10 10:08:38 +0300

    Conditional rendering changed in case of no app user

--- src/views/Profile/Profile.jsx
+++ src/views/Profile/Profile.jsx
@@ -16,7 +16,9 @@ export default function Profile({ token, logout }) {
     if (!token) return;
 
     getUserData(token, logout)
-      .then((data) => setUser(data))
+      .then((data) => {
+        setUser(data);
+      })
       .catch((error) => {
         toast.error(error.message);
       });
@@ -24,25 +26,25 @@ export default function Profile({ token, logout }) {
 
   return (
     <>
-      {user ? (
-        <div className="custom-scroll w-100 h-100 align-items-center">
+      {!user ? (
+        <div
+          className={`fs-2 m-5 p-5 border text-center align-items-center rounded
+      ${
+        isDark
+          ? `bg-${theme.color} text-${theme.textColor} border border-white`
+          : `bg-${theme.color} text-${theme.textColor} border border-dark`
+      }
+      `}
+        >
+          No user
+        </div>
+      ) : (
+        <div className="container custom-scroll w-100 h-100 align-items-center">
           <div className="d-flex px-1 mx-4 my-2 mb-4 gap-4">
             <ProfileCard user={user} />
             <UserPlaylists />
           </div>
           <UserTopTracks />
-        </div>
-      ) : (
-        <div
-          className={`fs-2 m-5 p-5 border text-center rounded
-        ${
-          isDark
-            ? `bg-${theme.color} text-${theme.textColor} border border-white`
-            : `bg-${theme.color} text-${theme.textColor} border border-dark`
-        }
-        `}
-        >
-          No user
         </div>
       )}
     </>
